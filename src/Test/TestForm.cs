20b0c493d755c977255f1ed97c6c365689112e34
BehzadKhosravifar <Behzad.khosravifar@gmail.com>
2015-10-09 12:50:09 +0330

    First Initial

---
+++ src/Test/TestForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AboutBox;
+
+namespace Test
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = new AboutBox.AboutForm(this.Icon.ToBitmap());
+            a.ShowDialog();
+
+            var b = new AboutBox1();
+            b.Show();
+        }
+    }
+}
